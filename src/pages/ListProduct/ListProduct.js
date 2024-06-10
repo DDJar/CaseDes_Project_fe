3c3fe861de09c4dc437b999280d433bbb82f6323
DDJar <89435268+DDJar@users.noreply.github.com>
2024-06-11 00:11:59 +0700

    fix : css

--- src/pages/ListProduct/ListProduct.js
+++ src/pages/ListProduct/ListProduct.js
@@ -255,7 +255,7 @@ const ListProduct = () => {
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
                     {sortOptions.map((option) => (
                       <Menu.Item key={option.name}>
                         {({ active }) => (
