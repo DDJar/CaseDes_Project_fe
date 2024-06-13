c0ae169aa24a670a2a59b56517dd48cbd1d73ec6
DDJar <89435268+DDJar@users.noreply.github.com>
2024-06-13 14:19:29 +0700

    update: home product

--- src/components/Products2/Products.jsx
+++ src/components/Products2/Products.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Heading from '../Shared/Heading';
 import ProductCard from './ProductCard';
 import { useEffect } from 'react';
