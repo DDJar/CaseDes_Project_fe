555a3716ae7bb0dff10a13c40a77028cbae4a377
DDJar <89435268+DDJar@users.noreply.github.com>
2024-07-18 13:53:59 +0700

    update: category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -15,7 +15,7 @@ const Category = () => {
                             <div className="mb-4">
                                 <p className="mb-[2px] text-gray-400">Enjoy</p>
                                 <p className="text-2xl font-semibold mb-[2px]">With</p>
-                                <p className="text-4xl xl:text-5xl font-bold opacity-20 mb-2">Earphone</p>
+                                <p className="text-4xl xl:text-5xl font-bold opacity-20 mb-2">Case</p>
                                 <Button text="Browse" bgColor={'bg-primary'} textColor={'text-white'} />
                             </div>
                         </div>
